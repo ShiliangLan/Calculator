fd008f1973674139f877707729f730142f6e5dce
lanshiliang <473280418@qq.com>
2015-12-26 23:53:04 +0800

    修改计算方式

--- app/src/main/java/com/example/lanshiliang/calculator/Data.java
+++ app/src/main/java/com/example/lanshiliang/calculator/Data.java
@@ -1,6 +1,9 @@
 package com.example.lanshiliang.calculator;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by lanshiliang on 2015/12/23.
@@ -15,6 +18,10 @@ public class Data {
         test.add('*');
         test.add('/');
         test.add('.');
+    }
+
+    public void multiply(){
+
     }
 
     public double compute(){
@@ -33,25 +40,52 @@ public class Data {
             return 0;
         }
         String[] s=this.toString().split("\\+|-|\\*|/");
+        ArrayList<Double> list=new ArrayList<>();//用来存储计算乘除后的所有数字
+        System.out.println(list.toString());
         int flag=0;
-        result=a* Double.parseDouble(s[flag]);
+        list.add(Double.parseDouble(s[0]));
+
         for (char c:arrayList){
+            //遍历集合，对乘除优先计算，其次吧剩余数字加入list内
+           // System.out.println(list);
             if(flag>s.length-2) break;
             switch (c){
                 case '+':
-                    result+= Double.parseDouble(s[flag+1]);
+                    list.add(Double.parseDouble(s[flag+1]));
+                    //result+= Double.parseDouble(s[flag+1]);
                     flag++;
                     break;
                 case '-':
-                    result-= Double.parseDouble(s[flag+1]);
+                    list.add(Double.parseDouble(s[flag+1]));
+                    //result-= Double.parseDouble(s[flag+1]);
                     flag++;
                     break;
                 case '*':
-                    result*= Double.parseDouble(s[flag+1]);
+                    list.set(list.size() - 1, list.get(list.size() - 1) * Double.parseDouble(s[flag + 1]));
+                   // System.out.println(list);
                     flag++;
                     break;
                 case '/':
-                    result/= Double.parseDouble(s[flag+1]);
+                    list.set(list.size() - 1, list.get(list.size() - 1) / Double.parseDouble(s[flag + 1]));
+                   // System.out.println(list);
+                    flag++;
+                    break;
+            }
+
+        }
+        flag=0;
+        result=a* list.get(0);
+        for (char c:arrayList){
+            //对list内的所有数字进行加减计算
+            System.out.println(list);
+            if(flag>(list.size()-2)) break;
+            switch (c){
+                case '+':
+                    result+= list.get(flag + 1);
+                    flag++;
+                    break;
+                case '-':
+                    result-= list.get(flag+1);
                     flag++;
                     break;
             }
@@ -83,7 +117,6 @@ public class Data {
         arrayList.add(c);
         return true;
     }
-
     @Override
     public String toString() {
         StringBuffer stringBuffer=new StringBuffer();
